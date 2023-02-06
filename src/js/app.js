643a09ab9823d066dedb458fc7e271316da22373
vaneto0o <123696310+vaneto0o@users.noreply.github.com>
2023-02-07 00:56:46 +0200

    new

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,19 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  document.body.addEventListener("click", () => {
+    const lenth = 5;
+    let text = 'sampleText';
+    
+    let count = 0;
+    for (let i = 0; i < lenth; i++)
+    {
+      let element = document.createElement('article');
+      count += lenth;
+      element.setAttribute('id', 'article' + count);
+      document.body.appendChild(element);
+      document.getElementById('article' + count).innerHTML = text;
+      document.getElementById('article' + count).classList.add("message");
+    }
   });
 });
